6efe6ab3f7bfac87f29975f879904a8a2c879584
ritikkumar-18 <rk.u416@gmail.com>
2024-12-01 12:41:57 +0530

    admin

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Newuser from './components/Newuser.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
-      <Route path='/newuser' element={<Newuser/>}/>
+      
     </Routes>
     
   
